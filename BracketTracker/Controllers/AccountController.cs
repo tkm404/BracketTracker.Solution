d621ee011ae53ec0254b6ee62d3d87177ef28e77
Tom McDowell <tmcdowellddhs@yahoo.com>
2023-05-30 11:34:17 -0700

    add logout
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- BracketTracker/Controllers/AccountController.cs
+++ BracketTracker/Controllers/AccountController.cs
@@ -4,55 +4,88 @@ using BracketTracker.Models;
 using System.Threading.Tasks;
 using BracketTracker.ViewModels;
 
-namespace BracketTracker.Controllers
+namespace BracketTracker.Controllers;
+
+public class AccountController : Controller
 {
-  public class AccountController : Controller
+  private readonly BracketTrackerContext _db;
+  private readonly UserManager<ApplicationUser> _userManager;
+  private readonly SignInManager<ApplicationUser> _signInManager;
+
+  public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BracketTrackerContext db)
   {
-    private readonly BracketTrackerContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
+    _userManager = userManager;
+    _signInManager = signInManager;
+    _db = db;
+  }
 
-    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BracketTrackerContext db)
+  public ActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Register()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<ActionResult> Register(RegisterViewModel model)
+  {
+    if (!ModelState.IsValid)
     {
-      _userManager = userManager;
-      _signInManager = signInManager;
-      _db = db;
+      return View(model);
     }
-
-    public ActionResult Index()
+    else
     {
-      return View();
-    }
-
-    public IActionResult Register()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public async Task<ActionResult> Register (RegisterViewModel model)
-    {
-      if (!ModelState.IsValid)
+      ApplicationUser user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
       {
-        return View(model);
+        return RedirectToAction("Index");
       }
       else
       {
-        ApplicationUser user = new ApplicationUser { UserName = model.Email };
-        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-        if (result.Succeeded)
+        foreach (IdentityError error in result.Errors)
         {
-          return RedirectToAction("Index");
+          ModelState.AddModelError("", error.Description);
         }
-        else
-        {
-          foreach (IdentityError error in result.Errors)
-          {
-            ModelState.AddModelError("", error.Description);
-          }
-          return View(model);
-        }
+        return View(model);
       }
     }
+  }
+
+  public ActionResult Login()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<ActionResult> Login(LoginViewModel model)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(model);
+    }
+    else
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+        return View(model);
+      }
+    }
+  }
+
+  [HttpPost]
+  public async Task<ActionResult> LogOff()
+  {
+    await _signInManager.SignOutAsync();
+    return RedirectToAction("Index");
   }
 }

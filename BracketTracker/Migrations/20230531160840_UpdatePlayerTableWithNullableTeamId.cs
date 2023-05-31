01957e37331ce9958e602e211ce7ac2eb24ae879
Tom McDowell <tmcdowellddhs@yahoo.com>
2023-05-31 10:19:13 -0700

    make TeamId foreign key for Player nullable
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

---
+++ BracketTracker/Migrations/20230531160840_UpdatePlayerTableWithNullableTeamId.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BracketTracker.Migrations
+{
+    public partial class UpdatePlayerTableWithNullableTeamId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- BracketTracker/Models/Player.cs
+++ BracketTracker/Models/Player.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 namespace BracketTracker.Models;
 
 public class Player
@@ -5,7 +6,7 @@ public class Player
   public int PlayerId { get; set; }
   public string Name { get; set; }
   public int Powerscore { get; set; }
-  public int TeamId { get; set; }
+  public int? TeamId { get; set; }
   public Team Team { get; set; }
   // public int Cost { get; set; }
 }

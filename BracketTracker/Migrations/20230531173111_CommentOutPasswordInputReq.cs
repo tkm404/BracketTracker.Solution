fdd6423d8b38014dfc6c13ac530d465735653ec8
Tom McDowell <tmcdowellddhs@yahoo.com>
2023-05-31 10:43:10 -0700

    comment out pwd input req
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

---
+++ BracketTracker/Migrations/20230531173111_CommentOutPasswordInputReq.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BracketTracker.Migrations
+{
+    public partial class CommentOutPasswordInputReq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
